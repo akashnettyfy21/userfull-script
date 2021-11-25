01719b5a7d52aac6a2e4603ef8befae9d19a666a
akashnettyfy21 <akash.nettyfy@gmail.com>
2021-11-25 11:28:46 +0530

    localhost project sharing

---
+++ sharing-host-to-other-system.php
@@ -0,0 +1,16 @@
+## How to access other project/locahost on browser using lan / command
+
+# Check ip from Main system device using cmd
+Cmd : ip a (main system - whose you want to access)
+
+# ping from system in which you want to show hosted project / localhost 
+
+CMD - ping (ip of server )
+
+# Now Change hosts file from the system
+Go to : C:\Windows\System32\drivers\etc\hosts
+
+Change in file : add ip & link of project url/uri
+Like : 
+
+192.168.29.172        magento243.com
